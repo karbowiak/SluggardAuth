731c8e12fa33da5eab211dc1337d817f5b8c5a8a
Michael Karbowiak <karbowiak@gmail.com>
2016-03-18 19:34:18 +0100

    Don't actually give an invite link to everyone.. those who don't have access shouldn't be allowed in.. duh

--- app.php
+++ app.php
@@ -72,21 +72,25 @@ $app->get("/auth/", function() use ($app, $config) {
         }
     }
 
-    // Generate an invite link
-    $discord = new \Discord\Discord($config["discord"]["email"], $config["discord"]["pass"]);
-    $inviteData = $discord->api("channel")->invites()->create($config["discord"]["inviteChannel"], 60 * (mt_rand(1, 500) + 30), 1);
-    $inviteLink = "https://discord.gg/" . $inviteData["code"];
+    if(!empty($access)) {
+        // Generate an invite link
+        $discord = new \Discord\Discord($config["discord"]["email"], $config["discord"]["pass"]);
+        $inviteData = $discord->api("channel")->invites()->create($config["discord"]["inviteChannel"], 60 * (mt_rand(1, 500) + 30), 1);
+        $inviteLink = "https://discord.gg/" . $inviteData["code"];
 
-    // Make the json access list
-    $accessList = json_encode($access);
+        // Make the json access list
+        $accessList = json_encode($access);
 
-    // Generate an auth string
-    $authString = uniqid();
+        // Generate an auth string
+        $authString = uniqid();
 
-    // Insert it all into the db
-    dbExecute("REPLACE INTO registrations (characterID, corporationID, allianceID, groups, authString, active) VALUES (:characterID, :corporationID, :allianceID, :groups, :authString, 1)", array(":characterID" => $characterID, ":corporationID" => $corporationID, ":allianceID" => $allianceID, ":groups" => $accessList, ":authString" => $authString));
+        // Insert it all into the db
+        dbExecute("REPLACE INTO registrations (characterID, corporationID, allianceID, groups, authString, active) VALUES (:characterID, :corporationID, :allianceID, :groups, :authString, 1)", array(":characterID" => $characterID, ":corporationID" => $corporationID, ":allianceID" => $allianceID, ":groups" => $accessList, ":authString" => $authString));
 
-    $app->render("authed.twig", array("inviteLink" => $inviteLink, "authString" => $authString));
+        $app->render("authed.twig", array("inviteLink" => $inviteLink, "authString" => $authString));
+    } else {
+        $app->render("noaccess.twig");
+    }
 });
 
 $app->run();
